91ee204e1dc35e5a6965ee45be23919f21e2992b
APerlinx <133282938+APerlinx@users.noreply.github.com>
2023-08-20 14:19:00 +0300

    Proj name mistake

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Coinbay-react-frontend",
+  base: "/Coinbay-react-fronend",
   plugins: [react()],
 })
